1a9f89132a66a0d117053c5b7d1e6daed9ebf327
Guille Carlos <g@guillecarlos.com>
2011-04-09 01:11:43 -0400

    README

--- ruby/client.rb
+++ ruby/client.rb
@@ -1,10 +1,7 @@
-require 'socket'  
-
-
-message = "<message from='northumberland@shakespeare.lit/westminster' id='richard2-4.1.247' to='kingrichard@royalty.england.lit/throne'><body>My lord, dispatch; read o'er these articles.</body></message>"
+require 'socket'
 
 Socket.tcp("localhost", 2000) {|sock|
-  sock.send message, 0
+  sock.send "GET /index.html HTTP/1.1", 0
   sock.close_write
   puts sock.read
 }
