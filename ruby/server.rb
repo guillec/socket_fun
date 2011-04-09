e3a7d752fe1cf3fc1879b0a776cf6632f7b8fa9b
Guille Carlos <g@guillecarlos.com>
2011-04-09 00:55:47 -0400

    Added the index.html file and made HTTP server to handle GET request

--- ruby/server.rb
+++ ruby/server.rb
@@ -1,13 +1,34 @@
 require 'socket'
 
-Socket.tcp_server_loop(2000) {|sock, client_addrinfo|
-  Thread.new {
-    begin
-      request = sock.readline
-      p request.to_s
-    ensure
-      sock.close
-    end
+Socket.tcp_server_loop("127.0.0.1", 2000){ |socket, client_addrinfo|
+  request = socket.readline
+  http_header = request.match(/GET .* HTTP\/1\.1/)
+  
+  unless (http_header)
+    socket.puts "HTTP/1.1 400 BAD REQUEST"
+    socket.close
+    next #restart loop
+  end
+  
+  file = request.split(' ')[1]
+  file = '.' + file
+  p file
+
+  unless ( File.exists?(file) )
+      socket.puts "HTTP/1.1 404 File Not Found"
+      socket.close
+      next
+  end
+
+  socket.puts "HTTP/1.1 200 OK\n"
+  socket.puts "Connection: close\n"
+  socket.puts "Content-Type: text/html\n"
+  File.open(file, 'r') { |f|
+      while (line = f.gets)
+          socket.puts line
+      end
   }
+
+  socket.close
 }
 
